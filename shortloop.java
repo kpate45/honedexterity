e998f4ee1632b1cbb9af4ca514f9287211a46dbf
kpate45 <43395202+kpate45@users.noreply.github.com>
2018-09-25 16:57:04 -0700

    Add files via upload

---
+++ shortloop.java
@@ -0,0 +1,19 @@
+class shortloop
+{
+    
+
+public static void main (String[] args) {
+
+    
+    int sum=0;
+    
+    for (int i=0;i<=40;i=i+2) sum+=i;
+    System.out.println("Sum is " + sum);
+    System.out.print("Sum Divided by two is " + sum/2);
+    
+            
+            
+            
+}
+
+}
