ffa02401f60ce2e819bf2c2cc7465ff932890003
kpate45 <43395202+kpate45@users.noreply.github.com>
2018-09-26 08:26:43 -0700

    Update shortloop.java

--- shortloop.java
+++ shortloop.java
@@ -7,9 +7,11 @@ public static void main (String[] args) {
     
     int sum=0;
     
-    for (int i=0;i<=40;i=i+2) sum+=i;
-    System.out.println("Sum is " + sum);
-    System.out.print("Sum Divided by two is " + sum/2);
+    for (int i=0;i<=40;i=i+2) sum+=i;           //short loop of tallying all numbers
+    
+    System.out.println("Sum is " + sum);        //Output of sum
+    
+    System.out.print("Sum Divided by two is " + sum/2);     //Displaying sum divided by two
     
             
             
